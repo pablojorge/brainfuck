c4cfc8f54d9edc34c933c95e40e725ac59ca5719
Pablo Jorge <pablohjorge@gmail.com>
2012-11-29 22:38:25 -0300

    Haskell interpreter version with lists manipulation instead of indexes

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,70 +1,54 @@
 import Data.Char
 import Debug.Trace
 
-alter :: [Int] -> Int -> (Int -> Int) -> [Int]
-alter mem ptr f = pre ++ [value] ++ post
-    where pre = take ptr mem
-          value = f $ mem!!ptr
-          post = drop (ptr+1) mem
-
-increment mem ptr = alter mem ptr (\x -> x+1)
-decrement mem ptr = alter mem ptr (\x -> x-1)
-
-loop prg mem ip ptr cond advance symbol match
-    | cond (mem!!ptr) = (fmatch (advance ip)) + 1
-    | otherwise = ip + 1
-    where fmatch ip
-            -- | trace (trace_fmatch ip) False = undefined
-            | prg!!ip == symbol = fmatch (advance (fmatch (advance ip)))
-            | prg!!ip == match = ip
-            | otherwise = fmatch (advance ip)
-          trace_fmatch ip =
+loop prg mem cond move symbol match
+    | cond (current mem) = advance (fmatch (move prg))
+    | otherwise = advance prg
+    where fmatch prg
+            -- | trace (trace_fmatch prg) False = undefined
+            | (current prg) == symbol = fmatch (move (fmatch (move prg)))
+            | (current prg) == match = prg
+            | otherwise = fmatch (move prg)
+          trace_fmatch prg =
             ("fmatch" ++  
-             " ip: " ++ show ip ++ 
-             " prg!!ip: " ++ show (prg!!ip)) 
+             " prg: " ++ show (current prg)) 
 
-loop_start :: [Char] -> [Int] -> Int -> Int -> Int
-loop_start prg mem ip ptr = loop prg mem ip ptr cond advance '[' ']'
-    where cond = (\x -> x == 0)
-          advance = (\x -> x + 1)
+loop_start prg mem = loop prg mem (\x -> x == 0) advance '[' ']'
+loop_end prg mem = loop prg mem (\x -> x > 0) recede ']' '['
 
-loop_end :: [Char] -> [Int] -> Int -> Int -> Int
-loop_end prg mem ip ptr = loop prg mem ip ptr cond advance ']' '['
-    where cond = (\x -> x > 0)
-          advance = (\x -> x - 1)
+current (a,b) = head b
 
-bf :: [Char] -> [Int] -> [Char] -> Int -> Int -> [Char]
-bf prg mem output ip ptr
+advance (a,b) = (a ++ [head b], tail b)
+recede (a,b) = (init a, (last a) : b)
+
+increment (a,b) = (a, ((head b) + 1) : (tail b))
+decrement (a,b) = (a, ((head b) - 1) : (tail b))
+
+bf :: ([Char], [Char]) -> ([Int], [Int]) -> [Char] -> [Char]
+-- return current output if we reached the end of the program
+bf (a,[]) mem output = output
+bf prg mem output
     -- | trace trace_bf False = undefined
-    -- return current output if we reached the end of the program
-    | ip >= (length prg) = output
-    -- increment/decrement data pointer
-    | prg!!ip == '>' = bf prg mem output (ip+1) (ptr+1)
-    | prg!!ip == '<' = bf prg mem output (ip+1) (ptr-1)
+    | (current prg) == '>' = bf (advance prg) (advance mem) output
+    | (current prg) == '<' = bf (advance prg) (recede mem) output
     -- increment/decrement pointed data
-    | prg!!ip == '+' = bf prg (increment mem ptr) output (ip+1) ptr
-    | prg!!ip == '-' = bf prg (decrement mem ptr) output (ip+1) ptr
+    | (current prg) == '+' = bf (advance prg) (increment mem) output
+    | (current prg) == '-' = bf (advance prg) (decrement mem) output
     -- input/output byte
-    | prg!!ip == '.' = bf prg mem (output ++ [chr (mem!!ptr)]) (ip+1) ptr
-    | prg!!ip == ',' = error "',' not supported" -- XXX support for ','
+    | (current prg) == '.' = bf (advance prg) mem (output ++ [chr (current mem)])
+    | (current prg) == ',' = error "',' not supported" -- XXX support for ','
     -- loops
-    | prg!!ip == '[' = bf prg mem output (loop_start prg mem ip ptr) ptr
-    | prg!!ip == ']' = bf prg mem output (loop_end prg mem ip ptr) ptr
+    | (current prg) == '[' = bf (loop_start prg mem) mem output
+    | (current prg) == ']' = bf (loop_end prg mem) mem output
     -- just ignore current character
-    | otherwise = bf prg mem output (ip+1) ptr
+    | otherwise = bf (advance prg) mem output
     where trace_bf = 
             ("bf" ++ 
-             " ip: " ++ show ip ++ 
-             " symbol: " ++ (showpos prg ip) ++ 
-             " ptr: " ++ show ptr ++
-             " value: " ++ (showpos mem ptr))
+             " prg: " ++ (show (current prg)) ++ 
+             " mem: " ++ (show (current mem)))
 
 -- Tracing
             
-showpos list pos 
-    | pos >= (length list) = "<OOB>"
-    | otherwise = show (list!!pos)
-
 -- Tests
 
 chr2bf :: Char -> [Char]
@@ -82,14 +66,11 @@ looptest = "++[>++[-]<-]."
 -- XXX lazy evaluation to see partial results
 -- XXX program name as argument, stdin program input
 --     http://learnyouahaskell.com/input-and-output#files-and-streams
--- XXX version with lists manipulation instead of indexes
 -- XXX ASM interpreter?
 
-interpreter input = show $ bf program mem output ip ptr
-    where program = input
-          mem = take 30000 (repeat 0)
+interpreter input = show $ bf program mem output
+    where program = ([], input)
+          mem = ([], take 30000 (repeat 0))
           output = []
-          ip = 0
-          ptr = 0
 
 main = interact interpreter
