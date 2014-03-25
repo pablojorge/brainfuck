59b53ccb4808b4eddebeea1e3ecbf43d9225592d
Pablo Jorge <pablohjorge@gmail.com>
2014-03-25 10:21:58 -0300

    Fixing indentation

--- javascript/brainfuck.js
+++ javascript/brainfuck.js
@@ -1,105 +1,105 @@
 function onOptimize() {
-	$('#optimized').val(optimize($('#program').val()));
+    $('#optimized').val(optimize($('#program').val()));
 }
 
 function onTranslate() {
-	$('#translated').val(translate($('#optimized').val()).join('\n'));
+    $('#translated').val(translate($('#optimized').val()).join('\n'));
 }
 
 function onRun() {
-	$('#output').val('');
+    $('#output').val('');
 
-	run($('#translated').val(), 
-		$("#input").val(), 
-		function (char) {
-	  		$('#output').val($('#output').val() + char);
-		});
+    run($('#translated').val(), 
+        $("#input").val(), 
+        function (char) {
+            $('#output').val($('#output').val() + char);
+        });
 }
 
 function onInterpret() {
-	$('#output').val('');
+    $('#output').val('');
 
-	interpret($('#program').val(), 
-			  $("#input").val(), 
-			  function (char) {
-		  		  $('#output').val($('#output').val() + char);
-			  });
+    interpret($('#program').val(), 
+              $("#input").val(), 
+              function (char) {
+                  $('#output').val($('#output').val() + char);
+              });
 }
 
 function optimize(program) {
-	/* remove invalid ops: */
-	var valid_op = function(op) {
-		return '-+<>[],.'.indexOf(op) > -1;
-	}
+    /* remove invalid ops: */
+    var valid_op = function(op) {
+        return '-+<>[],.'.indexOf(op) > -1;
+    }
 
-	program = program.split('').filter(valid_op).join('');
+    program = program.split('').filter(valid_op).join('');
 
-	return program;
+    return program;
 }
 
 function translate(program) {
-	var prologue = [
-		'var memory = {};',
-		'var ptr = 0;',
-		'for(var i = 0; i < 30000; ++i) {',
-		'	memory[i] = 0;',
-		'}',
-		'try {',
-	];
-	var body = [];
-	var epilogue = [
-		'} catch(e) {',
-		'} finally {',
-		'}',
-	];
-	var	opcodes = {
-		'>' : '++ptr;',
-		'<' : '--ptr;',
-		'+' : 'memory[ptr]++;',
-		'-' : 'memory[ptr]--;',
-		'.' : 'putchar(memory[ptr]);',
-		',' : 'memory[ptr] = getchar();',
-		'[' : 'while(memory[ptr] > 0) {',
-		']' : '}',
-	}
+    var prologue = [
+        'var memory = {};',
+        'var ptr = 0;',
+        'for(var i = 0; i < 30000; ++i) {',
+        '   memory[i] = 0;',
+        '}',
+        'try {',
+    ];
+    var body = [];
+    var epilogue = [
+        '} catch(e) {',
+        '} finally {',
+        '}',
+    ];
+    var opcodes = {
+        '>' : '++ptr;',
+        '<' : '--ptr;',
+        '+' : 'memory[ptr]++;',
+        '-' : 'memory[ptr]--;',
+        '.' : 'putchar(memory[ptr]);',
+        ',' : 'memory[ptr] = getchar();',
+        '[' : 'while(memory[ptr] > 0) {',
+        ']' : '}',
+    }
 
-	for(var i = 0; i < program.length; ++i) {
-		char = program.charAt(i);
-		if (char in opcodes) {
-			body.push(opcodes[char]);
-		}
-	}
+    for(var i = 0; i < program.length; ++i) {
+        char = program.charAt(i);
+        if (char in opcodes) {
+            body.push(opcodes[char]);
+        }
+    }
 
-	return prologue.concat(body).concat(epilogue);
+    return prologue.concat(body).concat(epilogue);
 }
 
 function run(program, input, output_cb) {
-	fun = new Function("getchar", "putchar", program);
+    fun = new Function("getchar", "putchar", program);
 
-	var in_idx = 0;
+    var in_idx = 0;
 
-	getchar = function() {
-		if (in_idx < input.length) {
-			return input.charCodeAt(in_idx++);
-		} else {
-			throw "EOF";
-		}
-	}
+    getchar = function() {
+        if (in_idx < input.length) {
+            return input.charCodeAt(in_idx++);
+        } else {
+            throw "EOF";
+        }
+    }
 
-	putchar = function(charCode) {
-		output_cb(String.fromCharCode(charCode));
-	}
+    putchar = function(charCode) {
+        output_cb(String.fromCharCode(charCode));
+    }
 
-	fun(getchar, putchar);
+    fun(getchar, putchar);
 }
 
 function interpret(program, input, output_cb) {
-	program = optimize(program);
+    program = optimize(program);
 
-	/* precompute jumps: */
-	var jumps = {};
-	for(var pc = 0, stack = []; pc < program.length; ++pc) {
-		var opcode = program[pc];
+    /* precompute jumps: */
+    var jumps = {};
+    for(var pc = 0, stack = []; pc < program.length; ++pc) {
+        var opcode = program[pc];
 
         if (opcode == '[') {
             stack.push(pc);
@@ -111,57 +111,57 @@ function interpret(program, input, output_cb) {
     }
 
     /* main interpreter loop: */
-	var memory = {0: 0},
-		ptr = 0,
-		in_ptr = 0,
-		pc = 0;
-	
-	for(var i = 0; i < 30000; ++i) {
-		memory[i] = 0;
-	}
+    var memory = {0: 0},
+        ptr = 0,
+        in_ptr = 0,
+        pc = 0;
+    
+    for(var i = 0; i < 30000; ++i) {
+        memory[i] = 0;
+    }
 
-	setInterval(function() {
-		INST_PER_CYCLE = 100000;
+    setInterval(function() {
+        INST_PER_CYCLE = 100000;
 
-		for(var i = 0; i < INST_PER_CYCLE && pc < program.length; i++) {
-			var opcode = program[pc];
-			switch(opcode) {
-				case '>':
-					++ptr;
-					break;
-				case '<':
-					--ptr;
-					break;
-				case '+':
-					memory[ptr]++;
-					break;
-				case '-':
-					memory[ptr]--;
-					break;
-				case '.':
-					output_cb(String.fromCharCode(memory[ptr]));
-					break;
-				case ',':
-					if (in_ptr < input.length) {
-						memory[ptr] = input.charCodeAt(in_ptr++);
-					} else {
-						return;
-					}
-					break;
-				case '[':
-					if (memory[ptr] == 0) { 
-						pc = jumps[pc];
-					}
-					break;
-	        	case ']':
-	            	if (memory[ptr] != 0) {
-	            		pc = jumps[pc];
-	            	}
-	            	break;
-				default:
-					throw ("unexpected opcode: " + opcode);
-			}
-			++pc;
-		}
-	}, 0);
+        for(var i = 0; i < INST_PER_CYCLE && pc < program.length; i++) {
+            var opcode = program[pc];
+            switch(opcode) {
+                case '>':
+                    ++ptr;
+                    break;
+                case '<':
+                    --ptr;
+                    break;
+                case '+':
+                    memory[ptr]++;
+                    break;
+                case '-':
+                    memory[ptr]--;
+                    break;
+                case '.':
+                    output_cb(String.fromCharCode(memory[ptr]));
+                    break;
+                case ',':
+                    if (in_ptr < input.length) {
+                        memory[ptr] = input.charCodeAt(in_ptr++);
+                    } else {
+                        return;
+                    }
+                    break;
+                case '[':
+                    if (memory[ptr] == 0) { 
+                        pc = jumps[pc];
+                    }
+                    break;
+                case ']':
+                    if (memory[ptr] != 0) {
+                        pc = jumps[pc];
+                    }
+                    break;
+                default:
+                    throw ("unexpected opcode: " + opcode);
+            }
+            ++pc;
+        }
+    }, 0);
 }
