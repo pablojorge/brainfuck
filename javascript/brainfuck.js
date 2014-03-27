1c561df9cb227cdec02dcce8870f7f87e12dc80d
Pablo Jorge <pablohjorge@gmail.com>
2014-03-27 02:55:42 -0300

    Removing old code

--- javascript/brainfuck.js
+++ javascript/brainfuck.js
@@ -1,65 +1,7 @@
 $(document).ready(function () {
-    // $("#btn-run").click(onRun);
-    // $("#btn-optimize").click(onOptimize);
-    // $("#btn-translate").click(onTranslate);
-    // $("#btn-interpret").click(onInterpret);
     $("#btn-start").click(onStart);
     $("#btn-stop").click(onStop);
 })
-
-// function onOptimize(event) {
-//     event.preventDefault();
-
-//     $('#optimized').val(optimize($('#program').val()));
-// }
-
-// function onTranslate(event) {
-//     event.preventDefault();
-
-//     $('#translated').val(translate($('#optimized').val()).join('\n'));
-// }
-
-// function onRun(event) {
-//     event.preventDefault();
-
-//     var output = [];
-//     var start = Date.now(), end;
-
-//     $('#output').val('');
-
-//     run($('#translated').val(), 
-//         $("#input").val(), 
-//         function (char) {
-//             output.push(char);
-//         });
-
-//     end = Date.now();
-
-//     $('#output').val(output.join(''));
-
-//     alert("Ran in " + ((end - start)/1000) + " seconds");
-// }
-
-// function onInterpret(event) {
-//     event.preventDefault();
-
-//     var start = Date.now(), end;
-
-//     $('#output').val('');
-
-//     interpret($('#program').val(), 
-//               $("#input").val(), 
-//               function (char) {
-//                   $('#output').val($('#output').val() + char);
-//               },
-//               function () {
-//                   start = Date.now();
-//               },
-//               function () {
-//                   end = Date.now();
-//                   alert("Ran in " + ((end-start)/1000) + " seconds");
-//               });
-// }
 
 var interpreter = undefined;
 
@@ -106,72 +48,6 @@ function onStop(event) {
     event.preventDefault();
     interpreter.stop();
 }
-
-// function optimize(program) {
-//     /* remove invalid ops: */
-//     var valid_op = function(op) {
-//         return '-+<>[],.'.indexOf(op) > -1;
-//     }
-
-//     program = program.split('').filter(valid_op).join('');
-
-//     return program;
-// }
-
-// function translate(program) {
-//     var prologue = [
-//         'var memory = {};',
-//         'var ptr = 0;',
-//         'for(var i = 0; i < 30000; ++i) {',
-//         '   memory[i] = 0;',
-//         '}',
-//         'try {',
-//     ];
-//     var body = [];
-//     var epilogue = [
-//         '} catch(e) {',
-//         '} finally {',
-//         '}',
-//     ];
-//     var opcodes = {
-//         '>' : '++ptr;',
-//         '<' : '--ptr;',
-//         '+' : 'memory[ptr]++;',
-//         '-' : 'memory[ptr]--;',
-//         '.' : 'putchar(memory[ptr]);',
-//         ',' : 'memory[ptr] = getchar();',
-//         '[' : 'while(memory[ptr] > 0) {',
-//         ']' : '}',
-//     }
-
-//     program.split('').forEach(function (char) {
-//         if (char in opcodes) {
-//             body.push(opcodes[char]);
-//         }
-//     })
-
-//     return prologue.concat(body).concat(epilogue);
-// }
-
-// function run(program, input, output_cb) {
-//     fun = new Function("getchar", "putchar", program);
-
-//     var in_idx = 0;
-
-//     getchar = function() {
-//         if (in_idx < input.length) {
-//             return input.charCodeAt(in_idx++);
-//         } else {
-//             throw "EOF";
-//         }
-//     }
-
-//     putchar = function(charCode) {
-//         output_cb(String.fromCharCode(charCode));
-//     }
-
-//     fun(getchar, putchar);
-// }
 
 function Interpreter(program, input, onStart, onTick, onFinish) {
     this.program = program;
