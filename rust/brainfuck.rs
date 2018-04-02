b5aacd53a1ff908dbab3163e06952329b75edced
Pablo Jorge <pablo.jorge@xapo.com>
2018-04-02 20:39:27 -0300

    Improve error handling in rust interpreter

--- rust/brainfuck.rs
+++ rust/brainfuck.rs
@@ -26,7 +26,14 @@ macro_rules! bf_loop {
     })
 }
 
-fn bf_jumps(program: &[u8]) -> HashMap<usize, usize> {
+#[derive(Debug)]
+enum InvalidProgramError {
+    ExcessiveOpeningBrackets(usize),
+    UnexpectedClosingBracket(usize),
+}
+
+fn bf_jumps(program: &[u8]) -> Result<HashMap<usize, usize>,
+                                      InvalidProgramError> {
     let mut stack: Vec<usize> = Vec::new();
     let mut jumps = HashMap::new();
 
@@ -34,7 +41,13 @@ fn bf_jumps(program: &[u8]) -> HashMap<usize, usize> {
         match opcode {
             '[' => stack.push(pc),
             ']' => {
-                let target = stack.pop().unwrap();
+                let target = match stack.pop() {
+                    Some(n) => Ok(n),
+                    None => Err(
+                        InvalidProgramError::
+                        UnexpectedClosingBracket(pc)
+                    )
+                }?;
                 jumps.insert(pc, target);
                 jumps.insert(target, pc);
             }
@@ -42,27 +55,50 @@ fn bf_jumps(program: &[u8]) -> HashMap<usize, usize> {
         }
     );
 
-    jumps
+    match stack.pop() {
+        Some(n) => Err(
+            InvalidProgramError::
+            ExcessiveOpeningBrackets(n)
+        ),
+        _ => Ok(jumps)
+    }
 }
 
 macro_rules! print_mem {
     ($mem:expr, $ptr:expr) => ({
         print!("{}", $mem[$ptr] as char);
-        io::stdout().flush().expect("Error flushing")
+        io::stdout().flush()?
     })
 }
 
 macro_rules! read_mem {
     ($mem:expr, $ptr:expr) => ({
-        io::stdin().read(&mut $mem[$ptr..$ptr+1])
-                   .expect("Failed to read char");
+        io::stdin().read(&mut $mem[$ptr..$ptr+1])?;
     })
 }
 
-fn bf_eval(program: &[u8]) {
+#[derive(Debug)]
+enum BFEvalError {
+    InvalidProgramError(InvalidProgramError),
+    IOError(std::io::Error),
+}
+
+impl std::convert::From<std::io::Error> for BFEvalError {
+    fn from(err: std::io::Error) -> BFEvalError {
+        BFEvalError::IOError(err)
+    }
+}
+
+impl std::convert::From<InvalidProgramError> for BFEvalError {
+    fn from(err: InvalidProgramError) -> BFEvalError {
+        BFEvalError::InvalidProgramError(err)
+    }
+}
+
+fn bf_eval(program: &[u8]) -> Result<(), BFEvalError> {
     let mut mem: [u8; 30000] = [0; 30000];
     let mut ptr: usize = 0;
-    let jumps = bf_jumps(program);
+    let jumps = bf_jumps(program)?;
 
     bf_loop!(opcode, pc, program,
         match opcode {
@@ -76,7 +112,9 @@ fn bf_eval(program: &[u8]) {
             ']' => if mem[ptr] != 0 {pc = jumps[&pc]},
             _ => (),
         }
-    )
+    );
+
+    Ok(())
 }
 
 fn main() {
@@ -89,5 +127,5 @@ fn main() {
     let program = read_file(&args[1]).expect("Error reading file");
     let program = program.as_bytes();
 
-    bf_eval(program);
+    bf_eval(program).expect("Error running program");
 }
